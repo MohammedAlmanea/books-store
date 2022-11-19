47d53d9f33d7e310ce65c171cb850d9fbe1c0eae
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-19 12:39:05 +0300

    Setting up testing env

--- src/database.ts
+++ src/database.ts
@@ -8,13 +8,27 @@ const{
     POSTGRES_DB,
     POSTGRES_USER,
     POSTGRES_PASSWORD,
+    ENV
 }= process.env
 
-const client = new Pool({
+let client
+
+if(ENV === 'test') {
+ client = new Pool({
+    host:POSTGRES_HOST,
+    database:POSTGRES_DB,
+    user:POSTGRES_USER,
+    password: POSTGRES_PASSWORD
+});
+}
+
+if(ENV === 'dev') {
+  client = new Pool({
     host:POSTGRES_HOST,
     database: POSTGRES_DB,
     user:POSTGRES_USER,
     password: POSTGRES_PASSWORD
 });
+}
 
 export default client;
