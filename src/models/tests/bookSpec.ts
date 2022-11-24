0111eba8fcf38c98e4f349142eeabee103635181
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-24 13:01:08 +0300

    Created tests for show method

--- src/models/tests/bookSpec.ts
+++ src/models/tests/bookSpec.ts
@@ -6,6 +6,15 @@ describe('BookStore model', () => {
   it('should have an index method', () => {
     expect(store.index).toBeDefined();
   });
+  it('should have an show method', () => {
+    expect(store.show).toBeDefined();
+  });
+  it('should have an create method', () => {
+    expect(store.create).toBeDefined();
+  });
+  it('should have an show method', () => {
+    expect(store.delete).toBeDefined();
+  });
 
   it('create method should add a book', async () => {
     const result = await store.create({
@@ -14,6 +23,17 @@ describe('BookStore model', () => {
       author: 'Katherine Paterson',
       summary: 'Childrens',
     });
+    expect(result).toEqual({
+      id: 1,
+      title: 'Bridge to Terabithia',
+      total_pages: 250,
+      author: 'Katherine Paterson',
+      summary: 'Childrens',
+    });
+  });
+
+  it('show method should show the correct book', async () => {
+    const result = await store.show('1');
     expect(result).toEqual({
       id: 1,
       title: 'Bridge to Terabithia',
