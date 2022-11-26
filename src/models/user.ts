937b90db659e4f9728851da218add2ece50654ae
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-26 14:22:28 +0300

    Added the rest of needed methods for user

--- src/models/user.ts
+++ src/models/user.ts
@@ -14,6 +14,36 @@ export type User = {
 };
 
 export class UserTable {
+  async index(): Promise<User[]> {
+    try {
+      const conn = await client.connect();
+      const sql = 'SELECT * FROM users';
+
+      const result = await conn.query(sql);
+
+      conn.release();
+
+      return result.rows;
+    } catch (err) {
+      throw new Error(`unable get users: ${err}`);
+    }
+  }
+
+  async show(id: string): Promise<User> {
+    try {
+      const sql = 'SELECT * FROM users WHERE id=($1)';
+      const conn = await client.connect();
+
+      const result = await conn.query(sql, [id]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`unable show user ${id}: ${err}`);
+    }
+  }
+
   async create(user: User): Promise<User[]> {
     try {
       const sql =
@@ -35,6 +65,23 @@ export class UserTable {
       return u;
     } catch (error) {
       throw new Error(`Can not create user ${error}`);
+    }
+  }
+
+  async delete(id: string): Promise<User> {
+    try {
+      const conn = await client.connect();
+      const sql = 'DELETE FROM users WHERE id=($1)';
+
+      const result = await conn.query(sql, [id]);
+
+      const product = result.rows[0];
+
+      conn.release();
+
+      return product;
+    } catch (err) {
+      throw new Error(`unable delete user (${id}): ${err}`);
     }
   }
 
