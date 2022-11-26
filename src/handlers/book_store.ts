09dfa8f6b06a60bfbcbd0e39fd9323f842d2c881
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-26 11:48:56 +0300

    Added show handler

--- src/handlers/book_store.ts
+++ src/handlers/book_store.ts
@@ -12,3 +12,14 @@ const index = async (_req: Request, res: Response) => {
         res.json(error);
     }
 }
+
+const show = async (req: Request, res: Response) => {
+    try {
+        const book = await store.show(req.params.id)
+        res.json(book)
+        
+    } catch (error) {
+        res.status(400);
+        res.json(error);
+    }
+}
