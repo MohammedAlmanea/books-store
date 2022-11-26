f86d731589edea847adbf3392a14b9713fc8eea2
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-26 11:49:36 +0300

    Completed book_store.ts handler

--- src/handlers/book_store.ts
+++ src/handlers/book_store.ts
@@ -23,3 +23,34 @@ const show = async (req: Request, res: Response) => {
         res.json(error);
     }
 }
+
+const create = async (_req: Request, res: Response) => {
+    try {
+        const book: Book = {
+            title: _req.body.title,
+            author: _req.body.author,
+            total_pages: _req.body.total_pages,
+            summary: _req.body.summary
+        }
+
+        const newBook = await store.create(book)
+        res.json(newBook)
+    } catch(err) {
+        res.status(400)
+        res.json(err)
+    }
+}
+
+const destroy = async (req: Request, res: Response) => {
+    const deleted = await store.delete(req.params.id)
+    res.json(deleted)
+}
+
+const bookRoutes = (app: express.Application) => {
+  app.get('/books', index)
+  app.get('/books/:id', show)
+  app.post('/books', create)
+  app.delete('/books/:id', destroy)
+}
+
+export default bookRoutes
