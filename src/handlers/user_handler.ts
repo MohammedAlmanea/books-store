0268a262f5159890e551134b33e72c93d3616f52
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-26 12:20:54 +0300

    Created user_handler.ts for user.ts

---
+++ src/handlers/user_handler.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+import { UserTable, User } from '../models/user';
+
+const Table = new UserTable();
+
+const create = async (_req: Request, res: Response) => {
+  try {
+    const user: User = {
+      first_name: _req.body.first_name,
+      last_name: _req.body.last_name,
+      user_name: _req.body.user_name,
+      password: _req.body.password,
+    };
+
+    const newUser = await Table.create(user);
+    res.json(newUser);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+const authenticate = async (_req: Request, res: Response) => {
+  try {
+    const username = _req.body.user_name;
+    const password = _req.body.password;
+
+    const auth = await Table.authenticate(username, password);
+    res.json(auth);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const userRoutes = (app: express.Application) => {
+  app.post('/books', create);
+  app.get('/books', authenticate);
+};
+
+export default userRoutes;
