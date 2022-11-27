4c79da2ee1717612217de6ef3864d5f7e55abaa1
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-11-27 18:07:03 +0300

    Created function addProduct to handle addProduct function from model order

--- src/handlers/orders_handler.ts
+++ src/handlers/orders_handler.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { Order, Order_Class} from '../models/order';
+import { Order, Order_Class } from '../models/order';
 
 const OrderObj = new Order_Class();
 
@@ -27,7 +27,7 @@ const create = async (req: Request, res: Response) => {
   try {
     const order: Order = {
       status: req.body.status,
-      user_id: req.body.user_id
+      user_id: req.body.user_id,
     };
 
     const newOrder = await OrderObj.create(order);
@@ -43,11 +43,30 @@ const destroy = async (req: Request, res: Response) => {
   res.json(deleted);
 };
 
+const addProduct = async (req: Request, res: Response) => {
+  const quantity: number = parseInt(req.params.quantity);
+  const order_id: string = req.params.order_id;
+  const product_id: string = req.params.product_id;
+
+  try {
+    const addedProduct = await OrderObj.addProduct(
+      quantity,
+      order_id,
+      product_id
+    );
+    res.json(addedProduct);
+  } catch (error) {
+    res.status(400);
+    res.json(error);
+  }
+};
+
 const OrdersRoutes = (app: express.Application) => {
   app.get('/orders', index);
   app.get('/orders/:id', show);
   app.post('/orders', create);
   app.delete('/orders/:id', destroy);
+  app.post('orders/:id/products', addProduct);
 };
 
 export default OrdersRoutes;
